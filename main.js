a916c1101b6e38b07b7a7e649e70cc148e5d6c50
YamezRS <james@wyler.com>
2016-02-08 16:42:56 -0700

    Remove Previous App
    Removed previous functionality from runtime loop

--- main.js
+++ main.js
@@ -29,9 +29,16 @@ var increasefreq = true;
 
 periodicActivity(); //call the periodicActivity function
 
+function ()
+{
+    
+}
+
 function periodicActivity()
 {
-    myOnboardLed.write(ledState?1:0); //if ledState is true then write a '1' (high) otherwise write a '0' (low)
+    
+    setTimeout
+    /*myOnboardLed.write(ledState?1:0); //if ledState is true then write a '1' (high) otherwise write a '0' (low)
     ledState = !ledState; //invert the ledState
     setTimeout(periodicActivity, timeout); //call the indicated function after 1 second (1000 milliseconds)
     
@@ -52,5 +59,5 @@ function periodicActivity()
         timeout += 100;
     }else{
         timeout -= 100;
-    }
+    }*/
 }
